87f9c1221e2a75f8dc0c8234dc29729a77079173
haudz1211 <110547937+haudz1211@users.noreply.github.com>
2022-08-14 22:19:51 +0700

    first commit

---
+++ code/js.java
@@ -0,0 +1,20 @@
+// var player;
+// function onYouTubePlayerAPIReady() {
+//     player = new YT.Player('container', {
+//         height: '390',
+//         width: '640',
+//         videoId: 'rh5YPwfIVOI',
+//         events: {
+//             'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+//         }
+//     });
+// }
+
+$(document).ready(function(){
+    $('a').click(function(){
+        
+    });
+
+
+});
